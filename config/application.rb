66182ba897f98ef37cd656fb08ce8631c3805ee5
Brett Bim <osiris43@gmail.com>
2014-09-19 13:09:24 -0500

    request headers maybe?

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,9 @@ module WhoShouldIBench
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.i18n.enforce_available_locales = false
+    config.action_dispatch.default_headers.merge!({
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    })
   end
 end
